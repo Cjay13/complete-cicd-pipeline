5fdae03051b9cdc4ac3f2aa18db1e5cdcf63c2e3
Charith <herath.charithjay@gmail.com>
2024-04-25 12:01:21 +0530

    fixed formatting

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "cjay-custom-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  azs              = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.3.0/24", "10.0.4.0/24"]
   database_subnets = ["10.0.5.0/24", "10.0.6.0/24"]
 
   create_database_subnet_group = true
